d4ba723f0bae096b48d6509f97b4c26e28f452cd
Fred <fmahiant@users.noreply.github.com>
2017-01-24 06:57:10 +0100

    Uses Thrift 0.9.3

--- installthrift.sh
+++ installthrift.sh
@@ -2,9 +2,9 @@
 set -e
 # check to see if thrift folder is empty
 if [ ! -d "$HOME/thrift/lib" ]; then
-  wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz;
-  tar xfz thrift-0.9.2.tar.gz;
-  cd thrift-0.9.2  &&  ./configure --without-java --without-c_glib --without-python --without-ruby --without-erlang --without-nodejs --without-go --prefix=$HOME/thrift && make install
+  wget http://archive.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz;
+  tar xfz thrift-0.9.3.tar.gz;
+  cd thrift-0.9.3  &&  ./configure --without-java --without-c_glib --without-python --without-ruby --without-erlang --without-nodejs --without-go --prefix=$HOME/thrift && make install
 else
   echo 'Using cached directory.';
 fi
